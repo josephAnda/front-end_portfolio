852e2e150aa7a2468173dc5504220b56b717b967
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-01-13 09:38:16 -0800

    updated readme

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,10 @@
 var menuBox = document.getElementById('menu-box');
 var navigation = document.getElementById('navlist');
 var opened = false;
-navigation.style.visibility = 'hidden';
+
+navigation.style.visibility = 'hidden';  //initialize drop-down nav element in an invisible state
+
+//Logic for the drop-down menu
 menuBox.addEventListener('click', function() {
 	open();
 });
@@ -11,14 +14,14 @@ function open() {
 		navigation.style.transition = 'all 0.5s ease';
 		navigation.style.transform = 'translatey(15px)';
 		navigation.style.visibility = 'visible';
-		navigation.style.display = 'inline';
+		//navigation.style.display = 'inline';  <--  Removed for transition.  Also, b/c visibility property handles this
 		opened = true;
 
 	} else {
 		navigation.style.transition = 'all 0.2s ease';
 		navigation.style.transform = 'translatey(-2px)';
 		navigation.style.visibility = 'hidden';
-		/*navigation.style.display = 'none'; <--- Removed for transition experiment */
+		//navigation.style.display = 'none'; <--- Removed for transition experiment 
 		opened = false;
 	}
 }
