9003dd5c4bcaef7ae7e3bc78c25fe7addef4add5
Joseph Anda <josephanda@Josephs-MBP.attlocal.net>
2016-01-16 23:43:59 -0800

    interface

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,52 @@ var menuBox = document.getElementById('menu-box');
 var navigation = document.getElementById('navlist');
 var opened = false;
 
+/*
+var box2 = document.getElementById('box2');
+var box3 = document.getElementById('box3');
+*/
+
+
+
+//  Below is an experiment to espouse DRY to the process of adding event listeners
+var boxes = document.querySelectorAll("#portfolio > div");
+var descriptions = document.querySelectorAll('.description')
+
+function fadeIn(box, element) {
+	box.addEventListener('mouseover', function() {
+		element.style.transition = 'opacity .25s ease-in-out';
+		element.style.opacity = '1';
+	});
+}
+
+function fadeOut(box, element) {
+	box.addEventListener('mouseout', function() {
+	element.style.transition = 'opacity .25s ease-in-out';
+	element.style.opacity = '0';
+	});
+}
+
+//description1.style.visibility = 'hidden';
+
 navigation.style.visibility = 'hidden';  //initialize drop-down nav element in an invisible state
 
 //Logic for the drop-down menu
 menuBox.addEventListener('click', function() {
 	open();
+});
+
+
+//fadeIn(box2, description2);
+//fadeOut(box2, description2);
+for (var i=0; i<boxes.length; i++) {
+	descriptions.item(i).style.opacity = '0';
+	fadeIn(boxes.item(i), descriptions.item(i));
+	fadeOut(boxes.item(i), descriptions.item(i));
+}
+box1.addEventListener('mouseout', function() {
+	description1.style.transition = 'opacity .25s ease-in-out';
+	description1.style.opacity = '0';
+	//description1.style.visibility = 'hidden';
 });
 
 function open() {
@@ -25,3 +66,4 @@ function open() {
 		opened = false;
 	}
 }
+
