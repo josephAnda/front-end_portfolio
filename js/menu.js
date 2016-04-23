75483858356e7468b351adebbbf49141b7db88e0
Joseph Anda <josephanda@Josephs-MBP.attlocal.net>
2016-04-22 21:58:07 -0700

    closer to MVC

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 
 (function() {
-
+	//  Finish refactoring this for MVC paradigm  
 	"use strict";
 	var view = {
 		menuBox:  document.getElementById('menu-box'),
@@ -15,56 +15,57 @@
     	],
     	current: 0,
     	changeBackground: function() {
-    		view.bio.css('background-image', backgrounds[current = ++current % backgrounds.length]);
+    		view.bio.css('background-image', view.backgrounds[view.current = ++view.current % view.backgrounds.length]);
     		setTimeout(view.changeBackground, 5000);
+    	},
+    	addMouseOvers: function() {
+    		
+    		//  Add mouseover effects for all item descriptions 
+			for (var i=0; i<view.boxes.length; i++) {
+				view.descriptions.item(i).style.opacity = '0';
+				fadeIn(view.boxes.item(i), view.descriptions.item(i));
+				fadeOut(view.boxes.item(i), view.descriptions.item(i));
+			}
+    	},
+    	init: function() {
+    		setTimeout(view.changeBackground, 5000);
+
+
+			//  Hide mobile nav initially + add menu logic
+			view.navigation.style.visibility = 'hidden';  
+			view.menuBox.addEventListener('click', function() {
+				open();
+			});
+			view.addMouseOvers();
     	}
 	}
-	//  Use the following variables:
-	var menuBox = document.getElementById('menu-box'),
-		navigation = document.getElementById('navlist'),
-		opened = false,
-		boxes = document.querySelectorAll("#portfolio > div"),
-		descriptions = document.querySelectorAll('.description'),
-		bio = $('#bio');
-	//  Holds the URLS for the backgreound images
-	var backgrounds = [
-      'url(css/img/guitar.jpg)', 
-      'url(css/img/mix.jpg)'
-    ];
-    //  Tracks the current backgorund image 
-    var current = 0;
+	var controller = {
+		init: function() {
+			view.init();
+		}
+	}
+
+
     
     //  The function and initiall call initiate the process of changing the background
     
-    setTimeout(view.changeBackground, 5000);
 
 
-	//  Hide mobile nav initially + add menu logic
-	navigation.style.visibility = 'hidden';  
-	menuBox.addEventListener('click', function() {
-		open();
-	});
 
-	//  Add mouseover effects for all item descriptions 
-	for (var i=0; i<boxes.length; i++) {
-		descriptions.item(i).style.opacity = '0';
-		fadeIn(boxes.item(i), descriptions.item(i));
-		fadeOut(boxes.item(i), descriptions.item(i));
-	}
 
 	//  Open and close 'menuBox' element on page
 	function open() {
-		if (!opened) {
-			navigation.style.transition = 'all 0.5s ease';
-			navigation.style.transform = 'translatey(15px)';
-			navigation.style.visibility = 'visible';
-			opened = true;
+		if (!view.opened) {
+			view.navigation.style.transition = 'all 0.5s ease';
+			view.navigation.style.transform = 'translatey(15px)';
+			view.navigation.style.visibility = 'visible';
+			view.opened = true;
 
 		} else {
-			navigation.style.transition = 'all 0.2s ease';
-			navigation.style.transform = 'translatey(-2px)';
-			navigation.style.visibility = 'hidden';
-			opened = false;
+			view.navigation.style.transition = 'all 0.2s ease';
+			view.navigation.style.transform = 'translatey(-2px)';
+			view.navigation.style.visibility = 'hidden';
+			view.opened = false;
 		}
 	}
 
@@ -84,4 +85,5 @@
 		});
 	}
 	console.log(bio);
+	controller.init();
 }) ();
