d8f3b1f2392580c8039b6f71688b93d063564419
Joseph Anda <josephanda@Josephs-MBP.attlocal.net>
2016-04-23 19:18:13 -0700

    completed refactor

--- js/menu.js
+++ js/menu.js
@@ -13,77 +13,61 @@
       		'url(css/img/guitar.jpg)', 
       		'url(css/img/mix.jpg)'
     	],
-    	current: 0,
-    	changeBackground: function() {
-    		view.bio.css('background-image', view.backgrounds[view.current = ++view.current % view.backgrounds.length]);
-    		setTimeout(view.changeBackground, 5000);
-    	},
-    	addMouseOvers: function() {
-    		
-    		//  Add mouseover effects for all item descriptions 
-			for (var i=0; i<view.boxes.length; i++) {
-				view.descriptions.item(i).style.opacity = '0';
-				fadeIn(view.boxes.item(i), view.descriptions.item(i));
-				fadeOut(view.boxes.item(i), view.descriptions.item(i));
-			}
-    	},
+    	current: 0,	   
     	init: function() {
-    		setTimeout(view.changeBackground, 5000);
-
-
 			//  Hide mobile nav initially + add menu logic
 			view.navigation.style.visibility = 'hidden';  
 			view.menuBox.addEventListener('click', function() {
-				open();
+				controller.open();
 			});
-			view.addMouseOvers();
     	}
 	}
 	var controller = {
 		init: function() {
 			view.init();
-		}
-	}
-
-
-    
-    //  The function and initiall call initiate the process of changing the background
-    
-
-
-
-
-	//  Open and close 'menuBox' element on page
-	function open() {
-		if (!view.opened) {
+			setTimeout(controller.changeBackground, 5000);
+			controller.addMouseOvers();
+		},
+		changeBackground: function() {
+    		view.bio.css('background-image', view.backgrounds[view.current = ++view.current % view.backgrounds.length]);
+    		setTimeout(controller.changeBackground, 5000);
+    	},
+		addMouseOvers: function() {
+    		
+    		//  Add mouseover effects for all item descriptions 
+			for (var i=0; i<view.boxes.length; i++) {
+				view.descriptions.item(i).style.opacity = '0';
+				controller.fadeIn(view.boxes.item(i), view.descriptions.item(i));
+				controller.fadeOut(view.boxes.item(i), view.descriptions.item(i));
+			}
+    	}, 
+    	open: function() {
+    		if (!view.opened) {
 			view.navigation.style.transition = 'all 0.5s ease';
 			view.navigation.style.transform = 'translatey(15px)';
 			view.navigation.style.visibility = 'visible';
 			view.opened = true;
 
-		} else {
-			view.navigation.style.transition = 'all 0.2s ease';
-			view.navigation.style.transform = 'translatey(-2px)';
-			view.navigation.style.visibility = 'hidden';
-			view.opened = false;
-		}
+			} else {
+				view.navigation.style.transition = 'all 0.2s ease';
+				view.navigation.style.transform = 'translatey(-2px)';
+				view.navigation.style.visibility = 'hidden';
+				view.opened = false;
+			}
+    	},
+    	fadeIn: function(box, element) {
+    		box.addEventListener('mouseover', function() {
+				element.style.transition = 'opacity .25s ease-in-out';
+				element.style.opacity = '1';
+			});
+    	},
+    	fadeOut: function(box, element) {
+    		box.addEventListener('mouseout', function() {
+				element.style.transition = 'opacity .25s ease-in-out';
+				element.style.opacity = '0';
+			});
+    	}
 	}
-
-	//  Show item description 
-	function fadeIn(box, element) {
-		box.addEventListener('mouseover', function() {
-			element.style.transition = 'opacity .25s ease-in-out';
-			element.style.opacity = '1';
-		});
-	}
-
-	//  Hide item description 
-	function fadeOut(box, element) {
-		box.addEventListener('mouseout', function() {
-		element.style.transition = 'opacity .25s ease-in-out';
-		element.style.opacity = '0';
-		});
-	}
-	console.log(bio);
+    //  The function and initiall call initiate the process of changing the background
 	controller.init();
 }) ();
